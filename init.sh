d361df1841ab4b7f98f988a840cc241b646d70a8
se000ra <se000ra@gmail.com>
2016-05-15 11:52:39 +0200

    Fixing symlink in init.sh

--- init.sh
+++ init.sh
@@ -7,5 +7,7 @@
 #
 
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
