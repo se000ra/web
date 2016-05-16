2e032a56a59c8d553ea085020e116c35edee3170
se000ra <se000ra@gmail.com>
2016-05-16 05:35:45 +0200

    Adding qury parsing

--- hello.py
+++ hello.py
@@ -5,10 +5,15 @@ def app(env,start_response):
     status = '200 OK'
     headers = [('Content-Type','text/html')]
     #body = 'hello'
+    #env = {}
+    env['QUERY_STRING'] = 'a=1&a=2&b=3'
     body = env['QUERY_STRING'].split('&')
-    print body
+    res = [i+'\r\n' for i in body]
+    #res
+    #print body
     start_response(status, headers)
     #return[body]
+    return[res]
     #return [bytes(data, 'utf-8')]
     #return [body.encode('utf-8')]
-    return 'hi'
+    #return 'hi'
