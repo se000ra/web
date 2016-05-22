3d1ccff5e3ea6b36c7e0f9e6f63f591dbc9533a1
se000ra <se000ra@gmail.com>
2016-05-22 12:50:18 +0200

    Cleaning up init

--- init.sh
+++ init.sh
@@ -5,9 +5,12 @@
 #
 # Distributed under terms of the MIT license.
 #
-
-
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#nginx conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+
+
+#gunicorn conf
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
