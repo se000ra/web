9c63ae6c95c3a3562a6e4f53c8b73451587e15d7
se000ra <se000ra@gmail.com>
2016-05-22 13:12:57 +0200

    Clean up hello

--- hello.py
+++ hello.py
@@ -1,19 +1,15 @@
 #test app
+from cgi import parse_qs
 
 #def wsgi_app(env,resp):
 def app(env,start_response):
+    query = parse_qs(env['QUERY_STRING'], keep_blank_values=1)
+    print query
+    res = [i+'\r\n' for i in query]
+    res = ''.join(res)
+
     status = '200 OK'
-    headers = [('Content-Type','text/html')]
-    #body = 'hello'
-    #env = {}
-    env['QUERY_STRING'] = 'a=1&a=2&b=3'
-    body = env['QUERY_STRING'].split('&')
-    res = [i+'\r\n' for i in body]
-    #res
-    #print body
+    headers = [('Content-Type','text/plain')]
     start_response(status, headers)
-    #return[body]
+    # return[body]
     return[res]
-    #return [bytes(data, 'utf-8')]
-    #return [body.encode('utf-8')]
-    #return 'hi'
