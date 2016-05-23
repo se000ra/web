44a9ad74d250399c9dfb2d9aa3c17631a0f73989
se000ra <se000ra@gmail.com>
2016-05-23 08:59:07 +0200

    test atomic

--- hello.py
+++ hello.py
@@ -2,10 +2,11 @@
 from cgi import parse_qs
 
 #def wsgi_app(env,resp):
+# just test of atomic-packge
 def app(env,start_response):
     query = parse_qs(env['QUERY_STRING'], keep_blank_values=1)
-    # res = [k+v+'\r\n' for  in query]
-    # res = ''.join(res)
+    res = [k+v+'\r\n' for  in query]
+    res = ''.join(res)
     body = []
     for key, values in query.items():
         for item in values:
